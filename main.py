e626f3b7682595c78b61417590a737bb6b83fb48
Parth Singh <65109271+Parth-AI@users.noreply.github.com>
2021-06-13 20:05:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 class ATM:
-     def __init__(self, cashToWithdraw, carNumber, ammountLeft = 1000000):
+     def __init__(self, carNumber, cashToWithdraw = 0, ammountLeft = 1000000):
           self.cashToWithdraw = cashToWithdraw
           self.carNumber = carNumber
           self.ammountLeft = ammountLeft-cashToWithdraw
      
-     def money(self):
+     def WithDrawCash(self):
           print(f"Your card number is {self.carNumber} cash withdrawn is {self.cashToWithdraw} and amount left is {self.ammountLeft}")
 
-Money = ATM(100000, 123456789)
-Money.money()
+     def BalanceLeft(self):
+          print(f"Your card number is {self.carNumber} and amount left is {self.ammountLeft}")
+
+a = input("Enter what you want to do:-\n\tWithDrawCash\n\tAmountleft\n\n ")
+if a == "WithDrawCash":
+     cwd = int(input("Enter the amount you want to withdraw:- "))
+     Money = ATM(123456789, cwd)
+     Money.WithDrawCash()
+elif a == "Amountleft":
+     Money = ATM(123456789)
+     Money.BalanceLeft()
